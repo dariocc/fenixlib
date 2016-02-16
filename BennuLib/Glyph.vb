f451954a84821dc3bd46e728669158afbf760d0c
dacucar <dario.cut@gmail.com>
2016-02-16 22:42:40 +0100

    Created own solution for BennuLib. Added Glyph class.

---
+++ BennuLib/Glyph.vb
@@ -0,0 +1,7 @@
+﻿<Serializable>
+Public Class Glyph
+
+    Public ReadOnly Property Width As Integer
+    Public ReadOnly Property Height As Integer
+    Public ReadOnly Property Pixels As IPixel()
+End Class
