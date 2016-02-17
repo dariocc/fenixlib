e5fdef716ed866d1df00d61b19fd927553be73db
dacucar <dario.cut@gmail.com>
2016-02-17 18:58:47 +0100

    Created MonochromePixel class to support B&W images.

---
+++ BennuLib.Sharp/Pixels/MonochromePixel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace BennuLib.Pixels
+{
+    class MonochromePixel : IPixel
+    {
+        public int Alpha
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Argb
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Blue
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Green
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool IsTransparent
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Red
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Value
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IPixel GetOpaqueCopy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPixel GetTransparentCopy()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
