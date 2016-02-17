360e9a904b49ffd58520e8b60df35398ae005764
dacucar <dario.cut@gmail.com>
2016-02-17 22:39:28 +0100

    Documented all functions, added SaveFpg function.

--- BennuLib.Sharp/File.cs
+++ BennuLib.Sharp/File.cs
@@ -9,48 +9,93 @@ namespace BennuLib.IO
 	public static class File
 	{
 
-		public static Sprite LoadMap(string fileName)
+        /// <summary>
+        /// Opens a Map file, reads all the information into a <see cref="Sprite"/> 
+        /// object, and then closes the file.
+        /// </summary>
+        /// <param name="path">The file to open.</param>
+        /// <returns>An instance of <see cref="Sprite"/> created from the file.</returns>
+		public static Sprite LoadMap(string path)
 		{
 			MapSpriteDecoder decoder = new MapSpriteDecoder();
 
-			using (var stream = System.IO.File.Open(fileName, FileMode.Open)) {
+			using (var stream = System.IO.File.Open(path, FileMode.Open)) {
 				return decoder.Decode(stream);
 			}
 		}
 
-		public static void SaveMap(Sprite sprite, string fileName)
+        /// <summary>
+        /// Creates a new Map file, writes the information of a <see cref="Sprite"/>,
+        /// and then closes the file. If the target file already exists, it is overwritten.
+        /// </summary>
+        /// <param name="sprite">The <see cref="Sprite"/> to write to the file.</param>
+        /// <param name="path">The file to write to.</param>
+		public static void SaveMap(Sprite sprite, string path)
 		{
 			MapSpriteEncoder encoder = new MapSpriteEncoder();
-			using (var output = System.IO.File.Open(fileName, FileMode.Create)) {
+			using (var output = System.IO.File.Open(path, FileMode.Create)) {
 				encoder.Encode(sprite, output);
 			}
 		}
 
-        public static SpriteAsset LoadFpg(string fileName)
+        /// <summary>
+        /// Opens a Fpg file, reads all the information into a <see cref="SpriteAsset"/> 
+        /// object, and then closes the file.
+        /// </summary>
+        /// <param name="path">The file to open.</param>
+        /// <returns>An instance of <see cref="SpriteAsset"/> created from the file.</returns>
+        public static SpriteAsset LoadFpg(string path)
         {
             FpgSpriteAssetDecoder decoder = new FpgSpriteAssetDecoder();
 
-            using (var stream = System.IO.File.Open(fileName, FileMode.Open))
+            using (var stream = System.IO.File.Open(path, FileMode.Open))
             {
                 return decoder.Decode(stream);
             }
         }
 
-        public static Palette LoadPal(string fileName)
+        /// <summary>
+        /// Creates a new Fpg file, writes the information of a <see cref="SpriteAsset"/>,
+        /// and then closes the file. If the target file already exists, it is overwritten.
+        /// </summary>
+        /// <param name="asset">The <see cref="SpriteAsset"/> to write to the file.</param>
+        /// <param name="path">The file to write to.</param>
+        public static void SaveFpg(SpriteAsset asset, string path)
+        {
+            FpgSpriteAssetEncoder encoder = new FpgSpriteAssetEncoder();
+            using (var output = System.IO.File.Open(path, FileMode.Create))
+            {
+                encoder.Encode(asset, output);
+            }
+        }
+
+        /// <summary>
+        /// Opens a 8bpp Pal, Map, Fpg or Fnt file, reads all the information into 
+        /// a <see cref="Palette"/> object, and then closes the file.
+        /// </summary>
+        /// <param name="path">The file to open.</param>
+        /// <returns>An instance of <see cref="Palette"/> created from the file.</returns>
+        public static Palette LoadPal(string path)
         {
             DivFormatPaletteDecoder decoder = new DivFormatPaletteDecoder();
 
-            using (var stream = System.IO.File.Open(fileName, FileMode.Open))
+            using (var stream = System.IO.File.Open(path, FileMode.Open))
             {
                 return decoder.Decode(stream);
             }
         }
 
-        public static void SavePal(Palette palette, string fileName)
+        /// <summary>
+        /// Creates a new Pal file, writes the information of a <see cref="Palette"/>,
+        /// and then closes the file. If the target file already exists, it is overwritten.
+        /// </summary>
+        /// <param name="palette">The <see cref="Palette"/> to write to the file.</param>
+        /// <param name="path">The file to write to.</param>
+        public static void SavePal(Palette palette, string path)
         {
             PalPaletteEncoder encoder = new PalPaletteEncoder();
 
-            using (var stream = System.IO.File.Open(fileName, FileMode.Open))
+            using (var stream = System.IO.File.Open(path, FileMode.Open))
             {
                 encoder.Encode(palette, stream);
             }
