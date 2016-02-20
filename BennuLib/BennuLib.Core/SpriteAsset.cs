b1f7898ead18e3d08f917af41d3d8853e14bf9f3
dacucar <dario.cut@gmail.com>
2016-02-20 17:59:25 +0100

    Better creation strategy for SpriteAssets.

--- BennuLib/BennuLib.Core/SpriteAsset.cs
+++ BennuLib/BennuLib.Core/SpriteAsset.cs
@@ -11,147 +11,146 @@ namespace BennuLib
 {
 
 
-	[Serializable()]
-	public sealed class SpriteAsset : IEnumerable<Sprite>
-	{
+    [Serializable ()]
+    public sealed class SpriteAsset : IEnumerable<Sprite>
+    {
 
 
-		private const int MinCode = 1;
-		private const int MaxCode = 999;
+        private const int MinCode = 1;
+        private const int MaxCode = 999;
 
-        public static SpriteAsset Create(DepthMode depthMode)
+        public static SpriteAsset Create ( DepthMode depthMode )
         {
-            switch (depthMode)
-            {
-                case DepthMode.ArgbInt32:
-                    return new SpriteAsset(32, null);
-                case DepthMode.RgbInt16:
-                    return new SpriteAsset(16, null);
-                case DepthMode.Monochrome:
-                    return new SpriteAsset(1, null);
-                default:
-                    throw new ArgumentException(); // TODO: Customize
-            }
+            return new SpriteAsset ( ( int ) depthMode );
         }
 
-        public static SpriteAsset Create(Palette palette)
+        public static SpriteAsset Create ( Palette palette )
         {
-            if (palette.Colors.Length != 256)
-                throw new ArgumentException();
+            if ( palette.Colors.Length != 256 )
+                throw new ArgumentException ();
 
-            return new SpriteAsset(8, palette);
+            return new SpriteAsset ( (int) DepthMode.RgbIndexedPalette, palette );
         }
 
-        private SpriteAsset(int depth, Palette palette) {
+        private SpriteAsset ( int depth, Palette palette = null)
+        {
             Depth = depth;
             Palette = palette;
         }
 
+        [Obsolete ()]
+        private bool IsIdValid ( int x )
+        {
+            return x >= MinCode & x <= MaxCode;
+        }
 
-		[Obsolete()]
-		private bool IsIdValid(int x)
-		{
-			return x >= MinCode & x <= MaxCode;
-		}
+        private IDictionary<int, Sprite> _sprites = new SortedDictionary<int, Sprite> ();
 
-		private IDictionary<int, Sprite> _sprites = new SortedDictionary<int, Sprite>();
+        public Sprite this[int code]
+        {
+            get { return _sprites[code]; }
+        }
 
-		public Sprite this[int code] {
-			get { return _sprites[code]; }
-		}
-
-		public Palette Palette { get; private set; }
+        public Palette Palette { get; private set; }
 
         public int Depth { get; private set; }
 
-		public ICollection<Sprite> Sprites {
-			get { return _sprites.Values; }
-		}
+        public ICollection<Sprite> Sprites
+        {
+            get { return _sprites.Values; }
+        }
 
-		public void Add(int code, ref Sprite sprite)
-		{
-            if (sprite.Depth != Depth)
-                throw new InvalidOperationException();
+        public void Add ( int code, ref Sprite sprite )
+        {
+            if ( sprite.Depth != Depth )
+                throw new InvalidOperationException ();
 
-			_sprites.Add(code, sprite);
-			sprite.ParentAsset = this;
-		}
+            _sprites.Add ( code, sprite );
+            sprite.ParentAsset = this;
+        }
 
-		public void Update(int code, Sprite map)
-		{
-			if (_sprites.ContainsKey(code)) {
-				_sprites.Remove(code);
-			}
+        public void Update ( int code, Sprite map )
+        {
+            if ( _sprites.ContainsKey ( code ) )
+            {
+                _sprites.Remove ( code );
+            }
 
-			_sprites.Add(code, map);
-		}
+            _sprites.Add ( code, map );
+        }
 
-		internal int IdOf(Sprite sprite)
-		{
-			foreach (KeyValuePair<int, Sprite> kvp in _sprites) {
-				if (object.ReferenceEquals(kvp.Value, sprite)) {
-					return kvp.Key;
-				}
-			}
+        internal int IdOf ( Sprite sprite )
+        {
+            foreach ( KeyValuePair<int, Sprite> kvp in _sprites )
+            {
+                if ( object.ReferenceEquals ( kvp.Value, sprite ) )
+                {
+                    return kvp.Key;
+                }
+            }
 
-			throw new ArgumentException();
-			// TODO customize
-		}
+            throw new ArgumentException ();
+            // TODO customize
+        }
 
-		public int FindFreeId(int startId = MinCode)
-		{
+        public int FindFreeId ( int startId = MinCode )
+        {
 
-			if (IsIdValid(startId))
-				throw new ArgumentException();
-			// TODO: Customize
+            if ( IsIdValid ( startId ) )
+                throw new ArgumentException ();
+            // TODO: Customize
 
-			var found = false;
-			var code = startId - 1;
-			do {
-				code += 1;
-				if (!_sprites.ContainsKey(code)) {
-					found = true;
-				}
-			} while (!(code == MaxCode | found));
+            var found = false;
+            var code = startId - 1;
+            do
+            {
+                code += 1;
+                if ( !_sprites.ContainsKey ( code ) )
+                {
+                    found = true;
+                }
+            } while ( !( code == MaxCode | found ) );
 
-			if (!found)
-				throw new InvalidOperationException();
-			// TODO: Customize 
+            if ( !found )
+                throw new InvalidOperationException ();
+            // TODO: Customize 
 
-			return code;
-		}
+            return code;
+        }
 
         [Obsolete]
-		public int PreviousFreeCode(int startId = MaxCode)
-		{
-			if (IsIdValid(startId))
-				throw new ArgumentException();
-			// TODO: Customize
+        public int PreviousFreeCode ( int startId = MaxCode )
+        {
+            if ( IsIdValid ( startId ) )
+                throw new ArgumentException ();
+            // TODO: Customize
 
-			var found = false;
-			var code = startId + 1;
-			do {
-				code -= 1;
-				if (!_sprites.ContainsKey(code)) {
-					found = true;
-				}
-			} while (!(code == MinCode | found));
+            var found = false;
+            var code = startId + 1;
+            do
+            {
+                code -= 1;
+                if ( !_sprites.ContainsKey ( code ) )
+                {
+                    found = true;
+                }
+            } while ( !( code == MinCode | found ) );
 
-			if (!found)
-				throw new InvalidOperationException();
-			// TODO: Customize 
+            if ( !found )
+                throw new InvalidOperationException ();
+            // TODO: Customize 
 
-			return code;
-		}
-
-		public IEnumerator<Sprite> GetEnumerator()
-		{
-			return _sprites.Values.GetEnumerator();
-		}
-
-		IEnumerator IEnumerable.GetEnumerator()
-		{
-			return GetEnumerator ();
+            return code;
         }
-	}
+
+        public IEnumerator<Sprite> GetEnumerator ()
+        {
+            return _sprites.Values.GetEnumerator ();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator ()
+        {
+            return GetEnumerator ();
+        }
+    }
 }
