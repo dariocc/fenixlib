64dcbd3784ba566b1beea7d0c94e992bda5e9984
dacucar <dario.cut@gmail.com>
2016-02-23 14:08:18 +0100

    Completed FntAbstractFontEncoder.

--- BennuLib/BennuLib.Core/IO/FntAbstractFontEncoder.cs
+++ BennuLib/BennuLib.Core/IO/FntAbstractFontEncoder.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-using static Bennu.IO.NativeFormat;
+﻿using static Bennu.IO.NativeFormat;
 
 namespace Bennu.IO
 {
@@ -8,9 +6,11 @@ namespace Bennu.IO
     {
         protected abstract int CodePageTypeForFont ( BitmapFont font );
 
-        protected abstract void WriteGlyphInfo ( ref GlyphInfo glypInfo, NativeFormatWriter writer );
+        protected abstract void WriteGlyphInfo ( ref GlyphInfo glypInfo, 
+            NativeFormatWriter writer );
 
-        protected override void WriteNativeFormatBody ( BitmapFont font, NativeFormatWriter writer )
+        protected override void WriteNativeFormatBody ( BitmapFont font, 
+            NativeFormatWriter writer )
         {
             if ( font.Depth == 8 )
             {
@@ -20,33 +20,40 @@ namespace Bennu.IO
 
             writer.Write ( CodePageTypeForFont ( font ) );
 
+            // The character data section needs to know in advance the start position of the 
+            // pixel data in the stream. Since the pixel of each glyph will be written 
+            // consecutively, we can calculate the relative position to the beginning of the
+            // Pixel data section by considering the width, height (and depth) of the 
+            // previous glyph.
+
+            // This information is gathered by the GlyphInfo structure, which will then be
+            // used by the WriteGlyphInfo function.
+
             GlyphInfo[] glyphsInfo = new GlyphInfo[256];
-            int dataOffset = 0; // Relative to the end of the glyp info section
-            for (int i = 0 ; i < glyphsInfo.Length ; i++ )
+
+            int pixelsDataOffset = 0; // Relative to the end of the glyph info section
+            for ( int i = 0 ; i < glyphsInfo.Length ; i++ )
             {
-                // TODO: Will fail if the character is not defined, in which case
-                // an empty glyph info shall be created
-                // TODO: Or might return null if this[] is configured as "element or default"
                 Glyph glyph;
-                try
+                if ( font[i] != null )
                 {
                     glyph = font[i];
                     glyphsInfo[i] = new GlyphInfo ( glyph.Width, glyph.Height,
-                        glyph.XOffset, glyph.YOffset, glyph.XAdvance, glyph.YAdavance, dataOffset );
-                } catch (Exception e)
-                {
-                    // Ignore missing characters
+                        glyph.XOffset, glyph.YOffset, glyph.XAdvance, glyph.YAdavance,
+                        pixelsDataOffset );
                 }
 
-                dataOffset += CalculatePixelBufferBytes(font.Depth, glyphsInfo[i].Width, 
-                    glyphsInfo[i].Height);
+                pixelsDataOffset += CalculatePixelBufferBytes ( font.Depth, 
+                    glyphsInfo[i].Width,
+                    glyphsInfo[i].Height );
 
-                // Delegate the actual writing of the glyph to the child classes
+                // Delegate the actual writing of the glyph to the child classes as it is
+                // different for Fnt or Fnx.
                 WriteGlyphInfo ( ref glyphsInfo[i], writer );
             }
 
-            // Write Pixel data for every glyph
-            foreach (Glyph g in font )
+            // Write pixel data for every glyph
+            foreach ( Glyph g in font )
             {
                 writer.Write ( g.PixelData );
             }
