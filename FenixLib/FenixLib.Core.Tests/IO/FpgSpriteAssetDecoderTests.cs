2a82e328c6870d0eae97a8e3ec3e3e1df54af8a9
dacucar <dario.cut@gmail.com>
2016-02-24 01:37:41 +0100

    Draft NUnit tests.

---
+++ FenixLib/FenixLib.Core.Tests/IO/FpgSpriteAssetDecoderTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FenixLib.IO;
+
+namespace FenixLib.Core.Tests.IO
+{
+    [TestFixture]
+    public class FpgSpriteAssetDecoderTests
+    {
+        [Test]
+        public void Decode_NullStream_IOException ()
+        {
+            FpgSpriteAssetDecoder decoder = new FpgSpriteAssetDecoder ();
+            decoder.Decode ( null );
+        }
+    }
+}
