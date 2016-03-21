16892abab44b18247b73ec43cfd0ee216eab1f2a
dacucar <dario.cut@gmail.com>
2016-03-21 08:59:50 +0100

    Layout changes.

--- FenixLib/Examples/DisplayingGraphicsInWindowsForms/Form1.Designer.cs
+++ FenixLib/Examples/DisplayingGraphicsInWindowsForms/Form1.Designer.cs
@@ -38,13 +38,15 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 76);
+            this.pictureBox1.Location = new System.Drawing.Point(14, 80);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(438, 452);
             this.pictureBox1.TabIndex = 0;
@@ -56,7 +58,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(346, 538);
+            this.button1.Location = new System.Drawing.Point(348, 542);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(104, 33);
             this.button1.TabIndex = 1;
@@ -68,7 +70,7 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(456, 76);
+            this.listBox1.Location = new System.Drawing.Point(458, 80);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(129, 452);
             this.listBox1.TabIndex = 2;
@@ -76,7 +78,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(485, 538);
+            this.button2.Location = new System.Drawing.Point(487, 542);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 33);
             this.button2.TabIndex = 3;
@@ -88,7 +90,7 @@
             // 
             this.listBox2.FormattingEnabled = true;
             this.listBox2.ItemHeight = 16;
-            this.listBox2.Location = new System.Drawing.Point(591, 76);
+            this.listBox2.Location = new System.Drawing.Point(593, 80);
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(129, 452);
             this.listBox2.TabIndex = 4;
@@ -96,7 +98,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(620, 538);
+            this.button3.Location = new System.Drawing.Point(622, 542);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(100, 33);
             this.button3.TabIndex = 5;
@@ -107,7 +109,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(457, 53);
+            this.label1.Location = new System.Drawing.Point(459, 57);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(52, 17);
             this.label1.TabIndex = 6;
@@ -116,7 +118,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(591, 53);
+            this.label2.Location = new System.Drawing.Point(593, 57);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(52, 17);
             this.label2.TabIndex = 7;
@@ -125,17 +127,38 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 53);
+            this.label3.Location = new System.Drawing.Point(15, 57);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(61, 17);
             this.label3.TabIndex = 8;
             this.label3.Text = "Preview:";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(533, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Use one of the \"Open\" buttons below to open a PixTudio / BennuGD / Div resource.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(250, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "FenixLib Windows Forms Example";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(734, 583);
+            this.ClientSize = new System.Drawing.Size(734, 591);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -165,6 +188,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
