fc773435c7d7c7519728e331a6b05d38410158e7
dacucar <dario.cut@gmail.com>
2016-07-01 09:42:16 +0200

    Change Moved ConcatenatedStream to own file and partially documented

--- FenixLib/FenixLib/IO/NativeDecoder.cs
+++ FenixLib/FenixLib/IO/NativeDecoder.cs
@@ -28,7 +28,7 @@ namespace FenixLib.IO
     /// implement.
     /// </summary>
     /// <remarks>
-    /// All native formats for fonts, graphics, graphic collections and palettes have 
+    /// All native formats for fonts, graphics, graphic collections and palettes have
     /// similarities in how they are read from disk:
     /// <list type="bullet">
     ///     <item>Might or might not be compressed in GZip format</item>
@@ -48,7 +48,7 @@ namespace FenixLib.IO
         public abstract int MaxSupportedVersion { get; }
 
         /// <summary>
-        /// Decodes the body of the native format and returns a <see cref="FenixLib"/> base 
+        /// Decodes the body of the native format and returns a <see cref="FenixLib"/> base
         /// type.
         /// </summary>
         /// <param name="header">A header object containing information of the magic, terminator
@@ -78,7 +78,7 @@ namespace FenixLib.IO
         /// will check if the argument matches that criteria.
         /// </summary>
         /// <param name="header">An array of bytes containing at least two elements</param>
-        /// <returns>True if the first two bytes <paramref name="header"/> are 
+        /// <returns>True if the first two bytes <paramref name="header"/> are
         /// that of GZip format.</returns>
 		protected static bool HeaderIsGZip ( byte[] header )
         {
@@ -109,12 +109,12 @@ namespace FenixLib.IO
 		public T Decode ( Stream input )
         {
             if ( input == null )
-            { 
+            {
                 throw new ArgumentNullException ( nameof ( input ) );
             }
 
             /* Native formats support GZip compression transparently
-             * This function will read the first two bytes of the file and determine if it 
+             * This function will read the first two bytes of the file and determine if it
              * is a GZip file, in which case it will use a GZipStream object to read it.
              */
 
@@ -193,7 +193,7 @@ namespace FenixLib.IO
         /// Returns whether the operation succeded or not.
         /// </summary>
         /// <param name="input">The input from which to read.</param>
-        /// <param name="decoded">When this method returns, the result of decoding the 
+        /// <param name="decoded">When this method returns, the result of decoding the
         /// <see cref="Stream"/>. If the decoding fails, <paramref name="decoded"/> will
         /// contain the default value of <typeparamref name="T"/>.</param>
         /// <returns>True if the decoding was successful. Otherwise false.</returns>
@@ -208,110 +208,6 @@ namespace FenixLib.IO
             {
                 decoded = default ( T );
                 return false;
-            }
-        }
-
-        private class ConcatenatedStream : Stream
-        {
-            private Stream stream1;
-            private Stream stream2;
-            private int _position;
-            private int _stream1Length;
-
-            public override bool CanRead { get; } = true;
-
-            public override bool CanSeek { get; } = false;
-
-            public override bool CanWrite { get; } = false;
-
-            /// <summary>
-            /// 
-            /// </summary>
-            /// <param name="stream1">Stream1 needs to support the Length property and it
-            /// cannot be longer than sizeof(int). Stream 2.</param>
-            /// <param name="stream2"></param>
-            public ConcatenatedStream ( Stream stream1, Stream stream2 )
-            {
-                _position = 0;
-
-                if ( stream1.Length > sizeof ( int ) )
-                    throw new ArgumentException (); // TODO:Customize
-
-                _stream1Length = ( int ) stream1.Length;
-
-                this.stream1 = stream1;
-                this.stream2 = stream2;
-            }
-
-            public override long Length
-            {
-                get
-                {
-                    throw new NotSupportedException ();
-                }
-            }
-
-            public override long Position
-            {
-                get
-                {
-                    return _position;
-                }
-
-                set
-                {
-                    throw new NotSupportedException ();
-                }
-            }
-
-            public override void Flush ()
-            {
-                throw new NotSupportedException ();
-            }
-
-            public override int Read ( byte[] buffer, int offset, int count )
-            {
-                byte[] bytes = new byte[count];
-
-                int bytesToReadFromStream1 = 0;
-                int bytesReadFromStream1 = 0;
-                int bytesReadFromStream2 = 0;
-
-                if ( Position < _stream1Length )
-                {
-                    if ( stream1.Length != _stream1Length )
-                        throw new InvalidOperationException (); // TODO: Customize: redimensionable stream1 is not supported
-
-                    int bytesLeftFromStream1 = _stream1Length - ( int ) stream1.Position;
-                    bytesToReadFromStream1 = Math.Min ( bytesLeftFromStream1, count );
-                    bytesReadFromStream1 = stream1.Read ( bytes, 0, bytesToReadFromStream1 );
-                }
-
-                if ( bytesReadFromStream1 < count && bytesReadFromStream1 == bytesToReadFromStream1 )
-                {
-                    bytesReadFromStream2 = stream2.Read ( bytes, bytesReadFromStream1,
-                        count - bytesReadFromStream1 );
-                }
-
-                bytes.CopyTo ( buffer, offset );
-
-                _position = bytesReadFromStream1 + bytesReadFromStream2;
-                return _position;
-            }
-
-            public override long Seek ( long offset, SeekOrigin origin )
-            {
-                throw new NotSupportedException ();
-            }
-
-            public override void SetLength ( long value )
-            {
-                throw new NotSupportedException ();
-            }
-
-            public override void Write ( byte[] buffer, int offset, int count )
-            {
-                throw new NotSupportedException ();
             }
         }
     }
